84b49289c5ddcc48c9ac3d17478794e12312e6eb
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-07-01 21:40:20 +0200

    redirecting backend

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,5 +1,4 @@
-const BEANSTALK_URL =
-  "https://chatter-env.eba-gxgabbz2.eu-north-1.elasticbeanstalk.com";
+const BEANSTALK_URL = "https://api.chatter.se";
 export const LOCAL_URL = "http://localhost:8000";
 
 export const BASE_URL = BEANSTALK_URL;
