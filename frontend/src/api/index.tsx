a056f46bca7d1bfe84350b0a5f5e6fe07cfaf88a
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-07-06 20:08:56 +0200

    subdomain

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,7 +1,5 @@
-export const BEANSTALK_URL =
-  "https://chatter-env.eba-gxgabbz2.eu-north-1.elasticbeanstalk.com";
 export const SUBDOMAIN_URL = "https://api.chatter.se";
 export const LOCAL_URL = "http://localhost:8000";
 
-export const BASE_URL = BEANSTALK_URL;
-export const WEBSOCKET_URL = BEANSTALK_URL.replace("https", "wss");
+export const BASE_URL = SUBDOMAIN_URL;
+export const WEBSOCKET_URL = SUBDOMAIN_URL.replace("https", "wss");
